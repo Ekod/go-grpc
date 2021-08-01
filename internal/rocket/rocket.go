38256c2c870e8903f3a37e0469ac0fc183accf06
Ekod <ekod87@yandex.ru>
2021-08-01 13:45:52 +0300

    Service created

---
+++ internal/rocket/rocket.go
@@ -0,0 +1,56 @@
+package rocket
+
+import "context"
+
+//Rocket - содержит определение рокеты
+type Rocket struct {
+	ID      string
+	Name    string
+	Type    string
+	Flights int
+}
+
+type Store interface {
+	GetRocketByID(id string) (Rocket, error)
+	InsertRocket(rkt Rocket) (Rocket, error)
+	DeleteRocket(id string) error
+}
+
+//Service - ответственен за обновление инвентаря рокеты
+type Service struct {
+	Store Store
+}
+
+//New - возвращает инстанс Сервиса
+func New(store Store) Service {
+	return Service{
+		Store: store,
+	}
+}
+
+// GetRocketByID - достаёт рокету из store по ID
+func (s Service) GetRocketByID(ctx context.Context, id string) (Rocket, error) {
+	rkt, err := s.Store.GetRocketByID(id)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+// AddRocket - Добавляет рокету в store
+func (s Service) AddRocket(ctx context.Context, rkt Rocket) (Rocket, error) {
+	rkt, err := s.Store.InsertRocket(rkt)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+// DeleteRocket - удаляет рокету - очень быстрый демонтаж
+func (s Service) DeleteRocket(id string) error {
+	err := s.Store.DeleteRocket(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
